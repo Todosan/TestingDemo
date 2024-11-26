8415c6e35dcc443f42bbc9a097c2618d50bfd399
Maryanne G <maryanneg@missionreadyhq.com>
2024-11-26 15:08:46 +1300

    set up server

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const carValueRoutes = require("./car-value-api/index");
 
 const app = express();
 
-// add your route after importing
+// add route to your api here
 app.use(carValueRoutes);
 
 const PORT = 3000;
